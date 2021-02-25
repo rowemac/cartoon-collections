fd47ced346ce4ab36155807a0ffb0cc07067bbe6
Mac Rowe <m.rowe42@gmail.com>
2021-02-25 18:23:46 -0500

    commit tree 534b1370144757b59d0073d8fc2da8dac5317ebc

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,48 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+require 'pry'
+
+def roll_call_dwarves(dwarves)
+  dwarves.each_with_index do | name, index |
+    puts "#{index + 1}. #{name}"
+  end
+
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(calls)
+  calls.map do | element |
+    element.capitalize + "!"
+  end
+
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls_long)
+  calls_long.select do | call |
+    if call.length < 4
+      return true
+    else 
+      call.length <= 4
+      return false
+    end
+  end
+
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+
+
+def find_the_cheese(array_1)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese_types.each do | cheese |
+    if array_1.include?(cheese)
+      return cheese
+    else 
+      return nil
+    end
+  end
 end
+
+
+
+
+
+
+
+
